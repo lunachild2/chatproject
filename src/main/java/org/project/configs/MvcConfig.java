bcbdbe6c834455c88574ce929495374e8206fa56
lunachild2 <gkgktjddus@gmail.com>
2023-09-21 21:52:15 +0900

    configs 추가

--- src/main/java/org/project/configs/MvcConfig.java
+++ src/main/java/org/project/configs/MvcConfig.java
@@ -1,10 +1,12 @@
 package org.project.configs;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableConfigurationProperties(FileConfig.class)
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
