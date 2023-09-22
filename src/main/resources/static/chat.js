228e7c6b8302db1c75d54df259ba594299577621
lunachild2 <gkgktjddus@gmail.com>
2023-09-22 19:44:54 +0900

    웹소켓 설정 테스트

--- src/main/resources/static/chat.js
+++ src/main/resources/static/chat.js
@@ -12,4 +12,11 @@ window.addEventListener("DOMContentLoaded", function() {
     webSocket.onmessage = function(message) {
         console.log("메세지 수신 : ", message);
     }
+
+    const sendButton = document.getElementById("send");
+    const messageEl = document.getElementById("message");
+    sendButton.addEventListener("click", function() {
+        const message = messageEl.value;
+        webSocket.send(message);
+    });
 });
