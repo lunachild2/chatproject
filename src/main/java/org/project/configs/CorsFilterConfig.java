b559724cd412576b896263ef72b0db8c0840004b
lunachild2 <gkgktjddus@gmail.com>
2023-09-26 19:07:44 +0900

    Cors 설정

--- src/main/java/org/project/configs/CorsFilterConfig.java
+++ src/main/java/org/project/configs/CorsFilterConfig.java
@@ -13,5 +13,12 @@ public class CorsFilterConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
     }
 }
