0d8d78172aa5df87f7d00571ded4b50e2cef336f
lunachild2 <gkgktjddus@gmail.com>
2023-10-04 21:00:22 +0900

    front 구현중

--- front_app/chatting_fe/src/outlines/Header.js
+++ front_app/chatting_fe/src/outlines/Header.js
@@ -1,23 +1,45 @@
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 import { MdHome } from 'react-icons/md'
+import fontSizes from "../styles/fontSizes";
 
 const OuterBox = styled.header`
     display: flex;
     justify-content: space-between;
-
-    .btns {
-        margin: 5px;
+    background: #000;
+    color: #fff;
+    height: 45px;
+    align-items: center;
+    padding: 0 10px;
+    * {
+        color: #fff;
+        font-size: ${ props => props.fontSize ? fontSizes[props.fontSize] : '1rem' };
     }
+    .btns {
+        a {
+            border: 1px solid #fff;
+            border-radius: 5px;
+            padding: 5px 10px;
+            transition: all 0.5s;
+        }
+        a+a { 
+            margin-left: 5px;
+        }
+    }
+    a:hover {
+        background: #fff;
+        color: #000;
+    }
+    
 `;
 
 const Header = () => {
     return (
-        <OuterBox>
+        <OuterBox fontSize="medium">
             <Link to="/"><MdHome /></Link>
 
             <div className="btns">
-                <Link to="/rooms">방목록</Link>
+                <Link to="/room">방목록</Link>
                 <Link to="/join">회원가입</Link>
                 <Link to="/login">로그인</Link>
             </div>
