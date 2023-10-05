81e7544ac1cbb5ced1b5daa7124f5a3b5067bd13
MJinHyuk <kitty0888@naver.com>
2023-10-05 20:54:50 +0900

    front 구현중

--- front_app/chatting_fe/src/pages/Rooms.js
+++ front_app/chatting_fe/src/pages/Rooms.js
@@ -1,10 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { getRooms } from "../api/chatting";
+import { useEffect, useState } from "react";
+import ErrorMessage from "../components/commons/ErrorMessage";
 
 const Rooms = () => {
+    const [rooms, setRooms] = useState([]);
+    const [message, setMessage] = useState('');
+    const [loading, setLoading] = useState(false);
+    
+    useEffect(() => {
+        getRooms()
+            .then((data) => {
+                console.log(data)
+                setRooms(data)
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+                setMessage("방목록 조회 실패...");
+                setLoading(false);
+            })
+    }, []);
+    
+    const lis = rooms.map(r => (<li key={r.roomNo}>{r.roomNM}</li>))
+
     return (
         <>
-            <h1>방 목록</h1>
-            <Outlet />
+            {message && <ErrorMessage>{message}</ErrorMessage>}
+            { loading && rooms.length === 0 && <div>로딩중...</div>}
+            <ul>
+                {lis}
+            </ul>
         </>
     );
 };
